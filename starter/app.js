9bd148e42f5a67dc494b3bc24eb165270ff093dd
PRABHANSHUBHOWAL <PRABHANSHUBHOWAL>
2023-10-24 20:53:07 +0530

    Added controllers and routes

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const port = 5000;
+app.use(express.static('./public'));
+
+const router=require('./routes/tasks');
+app.use('/api/v1',router);
+app.listen(port,()=>{
+    console.log(`Server running at port no. ${port}`);
+})
+
+
---
+++ starter/controllers/tasks.js
@@ -0,0 +1,17 @@
+
+const getAllTasks = (req,res) =>{
+    res.json({succesful:true});
+}
+const getTask = (req,res) =>{
+    res.json({succesful:true});
+}
+const createTask = (req,res) =>{
+    res.json({succesful:true});
+}
+const updateTask = (req,res) =>{
+    res.json({succesful:true});
+}
+const deleteTask = (req,res) =>{
+    res.json({succesful:true});
+}
+module.exports = {getAllTasks,getTask,createTask,updateTask,deleteTask};
