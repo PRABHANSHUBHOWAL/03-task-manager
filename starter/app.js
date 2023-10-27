8e0c5e86dd4915178e645122f7484d5cb24350e8
PRABHANSHUBHOWAL <PRABHANSHUBHOWAL>
2023-10-27 11:47:22 +0530

    Database connected

--- starter/app.js
+++ starter/app.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 const port = 5000;
-app.use(express.static('./public'));
 
-const router=require('./routes/tasks');
-app.use('/api/v1',router);
-app.listen(port,()=>{
-    console.log(`Server running at port no. ${port}`);
-})
+// middleware
+app.use(express.static('./public'));
+app.use(express.json())
+
+
+app.use('/api/v1',tasks);
+
+const start= async ()=>{
+    try {
+        await connectDB(process.env.mongo_uri);
+        app.listen(port,()=>{
+            console.log(`Server running at port no. ${port}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
+
 
 
--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,17 +1,47 @@
+const Task = require("../models/task");
 
-const getAllTasks = (req,res) =>{
-    res.json({succesful:true});
+const getAllTasks = async (req,res) =>{
+    try {
+        const task = await Task.find({});
+        res.json({task}); 
+    } catch (error) {
+        res.json({msg : "Generic Error found at getAllTasks"});
+    }
+    // res.json({succesful:true , data:"Get all Tasks"});
 }
-const getTask = (req,res) =>{
-    res.json({succesful:true});
+const getTask = async (req,res) =>{
+    try {
+        const task = await Task.findOne({_id : req.params.id});
+        res.json({task}); 
+    } catch (error) {
+        res.json({msg : "Generic Error found at getAllTasks"});
+    }
+    // res.json({succesful:true , data : req.params.id});
 }
-const createTask = (req,res) =>{
-    res.json({succesful:true});
+const createTask = async (req,res) =>{
+    try {
+        const task = await Task.create(req.body)
+        res.json({task}); 
+    } catch (error) {
+        res.json({msg : "Generic Error found at createTask"});
+    }
+    
 }
 const updateTask = (req,res) =>{
-    res.json({succesful:true});
+    
+    res.json({succesful:true, update:req.params.id});
 }
-const deleteTask = (req,res) =>{
-    res.json({succesful:true});
+const deleteTask = async (req,res) =>{
+    try {
+        const {id : taskid} =req.params;
+        const task = await Task.findOneAndDelete({_id : taskid})
+        if(!task)
+        return res.json({msg :"Task not found at deleteTask"});
+        // res.json({task}); 
+        res.json({succesful:true, delete:req.params.id ,task : task});
+    } catch (error) {
+        res.json({msg :"Generic Error found at deleteTask",error : error});
+    }
+    
 }
 module.exports = {getAllTasks,getTask,createTask,updateTask,deleteTask};
